d677269414c226ab4920101ce070a014c3ec3cf5
Ajay Kumar Chagam <ajaychagam@gmail.com>
2021-12-23 16:40:07 +0530

    added loader for API calls and media queries for responsive design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Entry } from 'contentful';
 import { ContentfulService } from './contentful.service';
+import { LoadingService } from './loading.service';
 
 @Component({
   selector: 'app-root',
@@ -11,16 +12,19 @@ export class AppComponent {
   title = 'y42-contentful-articles-angular';
   blogPosts: any[] = [];
   noOfRecords = 3;
+  loading$ = this.loader.loading$;
 
-  constructor(private contentfulService: ContentfulService) { }
+  constructor(private contentfulService: ContentfulService, private loader: LoadingService) { }
 
   async ngOnInit() {
     this.loadMore();
   }
 
   async loadMore() {
+    this.loader.show()
     const moreBlogs = await this.contentfulService.getProducts(this.noOfRecords, this.blogPosts.length);
     this.blogPosts = [...this.blogPosts, ...moreBlogs];
+    this.loader.hide()
   }
 
   trackByNo(index: number): number {
